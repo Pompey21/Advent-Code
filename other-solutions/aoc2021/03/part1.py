76b0dbe942b98776c6c6885227e4b4a39b9d3eaa
Pompey21 <marko.mekjavic@gmail.com>
2022-11-29 12:58:59 +0100

    ready for this year

--- other-solutions/aoc2021/03/part1.py
+++
@@ -1,8 +0,0 @@
-from diagreport import gamma_epsilon
-
-with open('input.txt') as f:
-    report = [line.strip() for line in f]
-
-gamma, epsilon = gamma_epsilon(report)
-
-print(int(gamma, 2) * int(epsilon, 2))
